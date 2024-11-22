03f6614007546c475c2bf74afb8d8c3ba19c5458
FilatEvgen <sergey.badyanov1@mail.ru>
2024-11-22 20:11:02 +0500

    add telegramClient

---
+++ redis-client-module/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinx.serialization)
+    id("com.google.devtools.ksp") version "2.0.21-1.0.26"
+    id("eu.vendeli.telegram-bot") version "7.4.1"
+
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.io.ktor.client.core)
+    implementation(libs.io.ktor.client.cio)
+    implementation(libs.io.ktor.client.websockets)
+    implementation(libs.io.ktor.client.content.negotiation)
+    implementation(libs.io.ktor.serialization.kotlinx.json)
+    implementation(libs.io.ktor.client.logging)
+    implementation("eu.vendeli:telegram-bot:7.4.1")
+    ksp("eu.vendeli:ksp:7.4.1")
+}
+kotlin {
+    jvmToolchain(21)
+}
